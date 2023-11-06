fa1f3ea8488c34c80261326993f750016756b7b3
nicolas-magalhaes-clear <nicolas.magalhaes.clear@gmail.com>
2023-11-06 12:25:17 -0300

    initial commit

---
+++ App.jsx
@@ -0,0 +1,31 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+
+
+
+
+export default function App() {
+
+
+
+  
+
+  return (
+    <View style={styles.container}>
+     
+    </View>
+  )
+}
+
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 30
+  }
+});
